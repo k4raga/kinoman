fc0605acfba9aaaf28a96e4a860d197ba1a885c3
k4raga <illkiri02@mail.ru>
2023-07-31 13:16:32 +0300

    start setting

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   // Указываем путь до входной точки:
@@ -6,8 +7,24 @@ module.exports = {
   // Описываем, куда следует поместить результат работы:
   output: {
     // Путь до директории (важно использовать path.resolve):
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "build"),
     // Имя файла со сборкой:
     filename: "bundle.js",
+    clean: true,
+  },
+  devtool: "source-map",
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: "public" }],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js/,
+        exclude: /(node_modules)/,
+        use: ["babel-loader"],
+      },
+    ],
   },
 };
